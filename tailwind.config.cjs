9be0e2bb6b80ac5b6aaedc961db375b257ee6a48
Nick A. Flores <Floresnick007gmail.com>
2023-03-19 18:35:27 -0700

    flexbox-grid and normalize added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,16 @@ module.exports = {
   content: [],
   theme: {
     extend: {},
+    colors: {
+      'onyx': '#393E41',
+      'timberwolf': '#D3D0CB',
+      'old-gold': '#E2C044',
+      'myrtle-green': '#587B7F',
+      'eerie-black': '#1E2019',
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+    },
   },
   plugins: [],
 }
