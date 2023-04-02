d3785747ed2b8053552dfac4b0424c15a6d1fee8
Nick A. Flores <Floresnick007gmail.com>
2023-04-02 01:38:10 -0500

    adding changes on deprecated items from flexboxgrid

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-
 function App() {
 
   return (
     <div>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-xs-6">columna 1</div>
-          <div className="col-xs-6">columna 2</div>
-        </div>
-      </div>
     </div>
   )
 }
